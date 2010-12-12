8397c983e0c28985c721d95a749f33c493d08f5f
brainfs <darknesssharp@gmail.com>
2010-12-12 16:40:44 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import setup
+
+desc = 'aojtools is a toolset for aoj, aizu online judge system(http://rose.u-aizu.ac.jp/onlinejudge/) written in Python, including transparently API acccess, problem submit tool and CLI script.')
+ldesc = desc
+
 setup(
     name='aojtools'
-    , version='0.0.1'
+    , version='0.1.0'
+    , description=desc
+    , long_description=ldesc
+    , classifiers = [
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+    , keywords='',
+    , author='yatt'
+    , author_email='darknesssharp@gmail.com'
+    , license='MIT Lisence'
 )
 
